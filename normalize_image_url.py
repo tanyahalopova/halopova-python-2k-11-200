6188de9d6bc6fff8604d185f4e91cdf937352b44
Tanya <tanyahalopova@gmail.com>
2023-10-24 22:58:54 +0500

    code design

--- normalize_image_url.py
+++ normalize_image_url.py
@@ -1,5 +1,3 @@
-import pytest
-
 def normalize_image_url(image_url, domain):
     if image_url.startswith('//'):
         image_url = 'http:' + image_url
@@ -13,10 +11,12 @@ def test_normalize_image_url_with_protocol():
     domain = 'example.com'
     assert normalize_image_url(image_url, domain) == 'https://example.com/image.jpg'
 
+
 def test_normalize_image_url_without_protocol():
     image_url = '//example.com/image.jpg'
     domain = 'example.com'
     assert normalize_image_url(image_url, domain) == 'http://example.com/image.jpg'
+
 
 def test_normalize_image_url_without_domain():
     image_url = '/image.jpg'
