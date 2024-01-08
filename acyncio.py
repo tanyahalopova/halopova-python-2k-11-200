e81efa1668ec3aabf18251ebe7a0029a45a2d593
Tanya <tanyahalopova@gmail.com>
2024-01-08 11:34:17 +0500

    +edits

--- acyncio.py
+++ acyncio.py
@@ -4,6 +4,8 @@ import os
 import aiohttp
 import bs4
 
+from normalize_image_url import normalize_image_url
+
 
 async def download_image(session, url, save_path):
     async with session.get(url) as response:
@@ -15,7 +17,7 @@ async def download_image(session, url, save_path):
                 file.write(chunk)
 
 
-async def scrape_images(url, save_dir):
+async def scrape_images(url, save_dir, domain):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             html = await response.text()
@@ -23,13 +25,14 @@ async def scrape_images(url, save_dir):
             img_urls = soup.find_all('img')
             tasks = []
             for img_url in img_urls:
-                image_url = img_url['src']
+                image_url = normalize_image_url(img_url['src'], domain)
                 save_path = os.path.join(save_dir, os.path.basename(image_url))
                 tasks.append(asyncio.create_task(download_image(session, image_url, save_path)))
-
         await asyncio.gather(*tasks)
 
 
 page_url = input("Введите адрес страницы: ")
+domain = input("Введите домен: ")
 save_directory = input("Введите название папки для сохранения изображений: ")
 os.makedirs(save_directory, exist_ok=True)
+asyncio.run(scrape_images(page_url, save_directory, domain))
